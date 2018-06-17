1584e85056d9725cff619406269ae6fd31e6bf2d
Dustin Schultz <dustindschultz@gmail.com>
2018-06-17 15:21:54 -0400

    added db config file

---
+++ schema/config.sql
@@ -0,0 +1,11 @@
+#ALTER USER 'root'@'localhost' IDENTIFIED BY '';
+#CREATE DATABASE gekkoga;
+#USE gekkoga;
+#CREATE USER 'root'@'localhost' IDENTIFIED BY '';
+#CREATE USER 'root'@'192.168.1.0/255.255.255.000' IDENTIFIED BY '';
+#ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '';
+#CREATE USER 'crypto'@'localhost' IDENTIFIED BY 'crypto';
+#RENAME USER 'root@192.168.1.115' TO 'root@192.168.1.0/255.255.255.000';
+#
+#GRANT ALL PRIVILEGES ON `gekkoga` TO `root`@'%' WITH GRANT OPTION;
+#GRANT ALL PRIVILEGES ON `gekkoga`.`*` TO `root`@`192.168.1.0/255.255.255.000` IDENTIFIED BY `` WITH GRANT OPTION;
