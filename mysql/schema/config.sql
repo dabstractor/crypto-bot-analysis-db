c59b0b30cfb75f744cee18ec925392fa92e235f0
Dustin Schultz <dustindschultz@gmail.com>
2018-06-19 16:33:09 -0400

    got mysql running with no pass on root and open to all IPs

---
+++ mysql/schema/config.sql
@@ -0,0 +1,20 @@
+ALTER USER 'root'@'localhost' IDENTIFIED BY '';
+-- GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
+CREATE USER 'root';
+CREATE DATABASE backtests;
+-- CREATE USER 'crypto';
+-- GRANT ALL PRIVILEGES ON backtests.* TO 'crypto'@'%' IDENTIFIED BY '' WITH GRANT OPTION;
+
+
+
+
+-- ALTER USER 'root'@'localhost' IDENTIFIED BY '';
+-- CREATE USER 'root'@'localhost' IDENTIFIED BY '';
+-- CREATE USER 'root'@'192.168.1.0/255.255.255.000' IDENTIFIED BY '';
+-- ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '';
+-- CREATE USER 'crypto'@'localhost' IDENTIFIED BY 'crypto';
+-- RENAME USER 'root@192.168.1.115' TO 'root@192.168.1.0/255.255.255.000';
+-- 
+-- GRANT ALL PRIVILEGES ON `gekkoga` TO `root`@'%' WITH GRANT OPTION;
+-- GRANT ALL PRIVILEGES ON `gekkoga`.`*` TO `root`@`192.168.1.0/255.255.255.000` IDENTIFIED BY `` WITH GRANT OPTION;
+-- GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '1234' WITH GRANT OPTION;
