8a1675c420a8be07319b8d9b084e73fc639648b7
Dustin Schultz <dustindschultz@gmail.com>
2018-06-28 20:48:55 -0400

    updated mysql user name because the other option was to drive to redwood city and kick Larry Ellison in the nutsack with a steel toe boot

--- mysql/schema/config.sql
+++ mysql/schema/config.sql
@@ -1,12 +1,10 @@
 CREATE DATABASE IF NOT EXISTS backtests;
 DROP USER IF EXISTS 'crypto';
-DROP USER IF EXISTS 'crypto'@'172.18.0.%';
-DROP USER IF EXISTS 'crypto'@'localhost';
+DROP USER IF EXISTS 'crypt';
 FLUSH PRIVILEGES;
-CREATE USER 'crypto'@'localhost'; -- cannot be accessed from outside of localhost. if server is compromised, database may as well be
-CREATE USER 'crypto'@'172.18.0.%';
+CREATE USER 'crypt'@'192.168.1.%'; -- cannot be accessed from outside of local network. if network (firewall) is compromised, database may as well be
 CREATE USER 'crypto' IDENTIFIED BY 'y9TxJd8LFmWFZ58p'; -- this account gets passworded because it can be accessed from anywhere
-GRANT ALL PRIVILEGES ON *.* TO 'crypto'@'172.18.0.%' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'crypt'@'192.168.1.%' WITH GRANT OPTION;
 GRANT ALL PRIVILEGES ON *.* TO 'crypto' IDENTIFIED BY 'y9TxJd8LFmWFZ58p' WITH GRANT OPTION;
-GRANT ALL PRIVILEGES ON backtests.* TO 'crypto'@'172.18.0.%' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON backtests.* TO 'crypt'@'192.168.1.%' WITH GRANT OPTION;
 GRANT ALL PRIVILEGES ON backtests.* TO 'crypto' IDENTIFIED BY 'y9TxJd8LFmWFZ58p' WITH GRANT OPTION;
